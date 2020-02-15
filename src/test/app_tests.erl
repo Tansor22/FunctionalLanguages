645802dda3a411cfb523d3f07a01c03f2a68c3de
Sergei <lyutyy.mail@gmail.com>
2020-02-15 11:54:47 +0700

    Usage of -import(module).

--- src/test/app_tests.erl
+++ src/test/app_tests.erl
@@ -10,14 +10,16 @@
 -author("Sergei").
 
 -include_lib("eunit/include/eunit.hrl").
+-import(app, [a/2, in/2, out/2]).
+-import(utils, [print/1, pretty_print/1]).
 
 a_0_0_test() ->
-  ?assertEqual(1, app:a(0, 0)).
+  ?assertEqual(1, a(0, 0)).
 a_2_1_test() ->
-  ?assertEqual(5, app:a(2, 1)).
+  ?assertEqual(5, a(2, 1)).
 a_0_1_test() ->
-  ?assertEqual(2, app:a(0, 1)).
+  ?assertEqual(2, a(0, 1)).
 a_1_1_test() ->
-  ?assertEqual(3, app:a(1, 1)).
+  ?assertEqual(3, a(1, 1)).
 a_2_3_test() ->
-  ?assertEqual(9, app:a(2, 3)).
+  ?assertEqual(9, a(2, 3)).
